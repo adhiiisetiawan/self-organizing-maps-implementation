4935382336b5cfbbeb3d6f6f80f2ab4ac519da91
Adhi Setiawan <adhisetiawan518@gmail.com>
2020-07-14 16:29:49 +0700

    add self organizing maps algorithm

--- SelfOrganizingMapsLearn.py
+++ SelfOrganizingMapsLearn.py
@@ -14,3 +14,13 @@ weight = [[0.2, 0.6, 0.5, 0.9],
 
 np_data = np.array(data)
 np_weight = np.array(weight)
+
+while epoch < maxEpoch:
+    for x in np_data:
+        euclidianDistance = [sum((w - x) ** 2) for w in np_weight]
+        minimum = np.argmin(euclidianDistance)
+        np_weight[minimum] += alpha * (x - np_weight[minimum])
+        print(np_weight)
+    alpha *= beta
+    epoch +=1
+
