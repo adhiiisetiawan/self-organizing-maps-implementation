9ed85c3aa374d1558488e680ae9c0001d753e6c9
Adhi Setiawan <adhisetiawan518@gmail.com>
2020-07-15 08:28:42 +0700

    try to add testing(beta)

--- SelfOrganizingMapsLearn.py
+++ SelfOrganizingMapsLearn.py
@@ -24,3 +24,12 @@ while epoch < maxEpoch:
     alpha *= beta
     epoch +=1
 
+dataTest = [[0.1, 0.2, 0.3, 0.4],
+            [0.5, 0.6, 0.7, 0.8],
+            [0.9, 0.10, 0.11, 0.12]]
+for xTest in dataTest:
+    testing = [sum((weightTesting - xTest) ** 2) for weightTesting in np_weight]
+    minimumTesting = np.argmin(testing)
+    print("===============Testing================")
+    print(testing[minimumTesting])
+
